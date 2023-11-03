50a9d9101da7dca06bbbaa07d7e4d521c372c216
alexander-slavchev <a.slavchev19.48@gmail.com>
2023-11-03 23:02:07 +0200

    DocsModelAndController

---
+++ OnlineTextEditor/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineTextEditor.Models;
+
+namespace OnlineTextEditor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Doc> Docs { get; set; }
+    }
+}
---
+++ OnlineTextEditor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineTextEditor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
